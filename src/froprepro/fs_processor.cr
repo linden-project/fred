4fd05335d1a0bf9f8efb59ff7a401a1d6814efc8
Pim Snel <pim@lingewoud.nl>
2019-08-16 17:01:37 +0200

    big refactor, single file key val change taskt

---
+++ src/froprepro/fs_processor.cr
@@ -0,0 +1,47 @@
+class FSProcessor
+
+  def initialize(dryrun)
+    @dryrun = false
+    @dryrun = dryrun
+  end
+
+  def rename_taxo_key_in_file(in_file, key_old, key_new)
+    in_file = File.expand_path(in_file)
+
+    if File.file?(in_file)
+      markdown_doc = MarkdownDoc.new(in_file)
+      markdown_doc.rename_taxo_key(key_old, key_new)
+
+     if @dryrun
+        markdown_doc.dump_markdown
+      else
+        puts "changing original file"
+        write_to_file(in_file, markdown_doc.markdown_string)
+      end
+    end
+  end
+
+  def rename_taxo_val_in_file(in_file, key, val_old, val_new)
+    in_file = File.expand_path(in_file)
+
+    if File.file?(in_file)
+      markdown_doc = MarkdownDoc.new(in_file)
+      markdown_doc.rename_taxo_val(key, val_old, val_new)
+
+     if @dryrun
+        markdown_doc.dump_markdown
+      else
+        puts "changing original file"
+        write_to_file(in_file, markdown_doc.markdown_string)
+      end
+    end
+  end
+
+  def write_to_file(out_file, contents)
+    out_file = File.expand_path(out_file)
+    file_h = File.open out_file, "w"
+    file_h.puts contents
+    file_h.close
+  end
+
+end
